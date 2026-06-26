2d8b12bdffbe83fb3b511ba578f46005992ab7b7
Yousef Mahrous <yousefzentral@gmail.com>
2026-06-26 21:26:10 +0300

    additional config

--- database.py
+++ database.py
@@ -5,6 +5,8 @@ import os
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+if DATABASE_URL and DATABASE_URL.startswith("postgresql://"):
+    DATABASE_URL = DATABASE_URL.replace("postgresql://", "postgresql+psycopg://", 1)
 
 # create_engine connects to your Neon database
 engine = create_engine(DATABASE_URL, echo=True)
